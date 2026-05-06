8071b1fa3c7a11679d623cf9616b5463d2b57254
SiYoung_Jung <jung@siyoung.dev>
2026-05-06 16:55:03 +0900

    dto 추가 application.properties 수정

--- plant-it-be/src/main/java/dev/siyoung/plantit/plantitbe/repository/PlantDiary.java
+++
@@ -1,6 +0,0 @@
-package dev.siyoung.plantit.plantitbe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlantDiary extends JpaRepository<PlantDiary, Long> {
-}
